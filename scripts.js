6ab8151433b69a037ceb1fc0756113844194fb05
mrogers17 <mrogers17@dashonline.org>
2016-10-07 16:38:56 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
+    $("#highschool").text(data.highschool);
     $.each(data.nickNames, function(key, value){
       $("#nicknames").append("<li>" + value.nick + "</li>");
     });
+
   });
 });
